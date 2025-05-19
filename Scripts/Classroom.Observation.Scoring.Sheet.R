e481a47cda5eaafc98c0384b012a08bf5117e4b0
Andrew Molo <molo.andrew@gmail.com>
2025-05-19 14:24:43 +0300

    Removed sensitive files/folders

--- Scripts/Classroom.Observation.Scoring.Sheet.R
+++
@@ -1,7 +0,0 @@
-# Borrow Datasets and libraries
-
-source("Scripts/Color.Harmonies.R")
-
-C0.Scoring.Sheet <- read_xlsx("Data/Primary classroom Observation Scoring Sheet  (created 2024-04-02) 2024-04-02.xlsx") %>%
-  filter(form.demographics.select_type_of_assessment %in% c("pilot_data_assessment", "irr"))
-
